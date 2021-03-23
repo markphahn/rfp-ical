3973b057ea2634975c322a34dda605e8417496f9
Mark Hahn <mhahn@tcbtech.com>
2021-03-23 12:42:56 -0700

    Added RFP class to encapsulate details.
    Added RFP class to hold details about RFP. Created CurrentRFP class
    to demonstrate how to use the RFP base class. Created MyCurrentRFP
    class to hold sensitive/private information about RFPs, and put this
    file in .gitignore so it is not checked into the public repositor.
    
    Updated documenation readme.

---
+++ rfp_dates/CurrentRFP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace rfp_dates {
+    internal class CurrentRFP : RFP {
+
+        public CurrentRFP() : base() {
+            target = "Example Target RFP";
+
+            description = "RFP for Example";
+            ownerEmail = "owner@example.com";
+
+            var currentTime = DateTime.Now.AddDays (3);
+            dueDate = new DateTime (currentTime.Year, currentTime.Month, currentTime.Day);
+        }
+    }
+}
---
+++ rfp_dates/RFP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace rfp_dates {
+    /// <summary>
+    /// Contains RFP information for creating a series of RFP ical events
+    /// </summary>
+    public abstract class RFP {
+
+        protected string target;
+        protected DateTime dueDate;
+        protected string description;
+        protected string ownerEmail;
+
+        public string Target { get { return target; } }
+        public DateTime DueDate { get { return dueDate; } }
+        public string Description { get { return description; } }
+        public string OwnerEmail { get { return ownerEmail; } }
+
+        public RFP() {
+            target = null;
+            dueDate = DateTime.MinValue;
+            description = null;
+            ownerEmail = null;
+        }
+    }
+}
